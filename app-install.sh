e755229e17d65892d1255a7d4619b5e61143b723
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-05-10 16:21:16 -0500

    fixed syntax issues

--- app-install.sh
+++ app-install.sh
@@ -6,11 +6,11 @@ fi
 
 sudo pacman -S --needed --noconfirm gvfs-smb p7zip unrar tar rsync file-roller terminator git
 sudo pacman -S --needed --noconfirm a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore
-sudo pacman -S --needed --noconfirm arc-gtk-theme papirus-icon-theme gnome-keyring copyq pinta nfs-tools
-sudo pacman -S --needed --noconfirm system-config-printer blueman networkmanager-openvpn hplip arandr
+sudo pacman -S --needed --noconfirm arc-gtk-theme papirus-icon-theme gnome-keyring copyq pinta nfs-utils
+sudo pacman -S --needed --noconfirm system-config-printer blueman networkmanager-openvpn hplip arandr lsof
 sudo pacman -S --needed --noconfirm remmina freerdp gnome-system-monitor maui-pix audacious okular gnome-screenshot
-sudo pacman -S --needed --noconfirm ncdu htop btop putty bat fzf tldr exa gprename nomacs ristretto flameshot pacman-contrib
-sudo pacman -S --needed --noconfirm gnome-characters obsidian-icon-theme deepin-icon-theme nfs-utils inotify-tools gnome-terminal
+sudo pacman -S --needed --noconfirm ncdu htop btop putty bat fzf tldr exa gprename ristretto flameshot pacman-contrib
+sudo pacman -S --needed --noconfirm gnome-characters obsidian-icon-theme deepin-icon-theme inotify-tools gnome-terminal
 sudo pacman -S --needed --noconfirm libreoffice-fresh hunspell hunspell-en_us mediainfo-gui mpv notepadqq foliate ebook-tools
 sudo pacman -S --needed --noconfirm cups cups-pdf czkawka mullvad-vpn
 sudo systemctl enable --now cups
