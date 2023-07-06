16e23992220190b6c9d675f91a620d260b6a5c2c
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-07-05 21:02:05 -0500

    Delete details.sh

--- details.sh
+++
@@ -1,8 +0,0 @@
-cp -r Wallpapers ~/Pictures/
-cp ./arch-shell/.* ~
-cat fstab.txt | sudo tee -a /etc/fstab
-sudo mkdir /media
-sudo mkdir /media/pinky
-sudo mkdir /media/share
-sudo mkdir /media/torrent
-sudo mount -a
