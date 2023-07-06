7496664c57cce8411e9076ec14bb25f19911b06f
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-07-05 21:04:36 -0500

    fresh update

---
+++ app-install.sh
@@ -0,0 +1,30 @@
+if : >/dev/tcp/8.8.8.8/53; then
+  echo 'Internet available.'
+else
+  echo 'Offline.';exit
+fi
+
+sudo pacman -S --needed --noconfirm gvfs-smb p7zip unrar tar rsync file-roller terminator git
+sudo pacman -S --needed --noconfirm a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore
+sudo pacman -S --needed --noconfirm arc-gtk-theme papirus-icon-theme gnome-keyring copyq pinta nfs-utils
+sudo pacman -S --needed --noconfirm system-config-printer blueman networkmanager-openvpn hplip arandr lsof mint-artwork
+sudo pacman -S --needed --noconfirm remmina freerdp gnome-system-monitor maui-pix audacious okular gnome-screenshot
+sudo pacman -S --needed --noconfirm ncdu htop btop putty bat fzf tldr exa gprename ristretto flameshot pacman-contrib
+sudo pacman -S --needed --noconfirm gnome-characters obsidian-icon-theme deepin-icon-theme inotify-tools gnome-terminal
+sudo pacman -S --needed --noconfirm libreoffice-fresh hunspell hunspell-en_us mediainfo-gui mpv notepadqq foliate ebook-tools
+sudo pacman -S --needed --noconfirm cups cups-pdf czkawka mullvad-vpn
+sudo systemctl enable --now cups
+yay -S google-chrome
+yay -S ttf-ms-fonts
+yay -S gnome-disk-utility
+yay lightdm-slick-greeter
+yay lightdm-settings
+yay -S pamac-aur
+yay -S albert
+#yay -S mullvad-vpn
+yay -S timeshift
+yay -S timeshift-autosnap
+yay -S netdiscover
+yay -S mp3gain
+
+
