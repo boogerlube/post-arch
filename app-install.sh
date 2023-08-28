14bcd4b0b6a20f6d8f568debd3e934e0c31fed25
boogerlube <bob@languy.com>
2023-08-28 18:46:29 -0500

    updated pkgs

--- app-install.sh
+++ app-install.sh
@@ -9,12 +9,12 @@ sudo pacman -S --needed --noconfirm a52dec faac faad2 flac jasper lame libdca li
 sudo pacman -S --needed --noconfirm arc-gtk-theme papirus-icon-theme gnome-keyring copyq pinta nfs-utils
 sudo pacman -S --needed --noconfirm system-config-printer blueman networkmanager-openvpn hplip arandr lsof
 sudo pacman -S --needed --noconfirm remmina freerdp gnome-system-monitor maui-pix audacious okular gnome-screenshot
-sudo pacman -S --needed --noconfirm ncdu htop btop putty bat fzf tldr exa gprename ristretto flameshot pacman-contrib
+sudo pacman -S --needed --noconfirm ncdu gdu htop btop putty bat fzf tldr exa gprename ristretto flameshot pacman-contrib
 sudo pacman -S --needed --noconfirm gnome-characters obsidian-icon-theme deepin-icon-theme inotify-tools gnome-terminal
 sudo pacman -S --needed --noconfirm libreoffice-fresh hunspell hunspell-en_us mediainfo-gui mpv notepadqq foliate ebook-tools
 sudo pacman -S --needed --noconfirm cups-pdf czkawka mullvad-vpn gnome-calculator gnome-clocks mlocate sshfs
 sudo pacman -S --needed --noconfirm yt-dlp rtmpdump atomicparsley aria2 python-mutagen python-pycryptodomex python-websockets
-sudo systemctl enable --now cups
+#sudo systemctl enable --now cups
 sudo pacman -S --needed --noconfirm google-chrome
 sudo pacman -S --needed --noconfirm ttf-ms-fonts
 sudo pacman -S --needed --noconfirm gnome-disk-utility
